468db68ad81b2f89f40592f35c18e25e9e5f06bb
daliDeli <bruns8900@gmail.com>
2018-11-30 16:40:15 +0100

    disabled buttons and input until text is submited

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,8 +5,13 @@ app.controller('MainController', function($scope){
   $scope.wordIsSubmited = false;
   $scope.sentenceIsSubmited = false;
   $scope.result = '';
-  $scope.submitSentence = () => { $scope.sentenceIsSubmited = true};
-  $scope.calculate = () => { 
+  $scope.submitSentence = () => { 
+    if($scope.sentence){
+      $scope.sentenceIsSubmited = true;
+    }
+    console.log($scope.sentenceIsSubmited);
+  };
+  $scope.calculateRepetition = () => { 
     $scope.repeats = $scope.sentence.split(' ').filter( word => word === $scope.word).length;
     $scope.result = $scope.repeats ? `Da nalazi se rec u textu ${$scope.repeats} puta.` : 'Ne nalazi se rec u textu.'
     $scope.wordIsSubmited = true;
@@ -16,5 +21,7 @@ app.controller('MainController', function($scope){
     $scope.word = '';
     $scope.sentenceIsSubmited = false;
     $scope.wordIsSubmited = false;
+    $scope.result = '';
+    $scope.repeats = 0;
   }
 });
